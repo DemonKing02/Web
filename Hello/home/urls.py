3b0d808c0df6470302b9df32c426d54b92322b9c
DemonKing02 <88969993+DemonKing02@users.noreply.github.com>
2023-02-04 17:47:47 +0530

    Add files via upload

---
+++ Hello/home/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path("", views.index, name='home'),
+    path("about", views.about, name='about'),
+    path("skills", views.skills, name='skills'),
+    path("contact", views.contact, name='contact'), 
+]
